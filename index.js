c2c4c14d7fdd8be545b52c5e52566e4177cbd1cf
sivanantham1998 <curlysiva@gmail.com>
2024-01-27 18:09:25 +0530

    skills div complete

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ menuBar.addEventListener('click',function()
       ul.style.display="block"
    }
 })
+
+// skills hide and show
+
+document.getElementById('sk').addEventListener('click',function()
+{
+   console.log('work')
+   let skills=document.getElementById('extra')
+
+   if(skills.style.display=="block")
+   {
+      skills.style.display='none'
+   }
+   else
+   {
+      skills.style.display="block"
+   }
+})
